5d8a31c307fef265b4a251b686a21b187d48e37f
Uzair Ejaz <uzairejaz987@gmail.com>
2025-10-25 01:03:28 +0500

    Initial commit

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -1,5 +1,5 @@
 import { React , useState} from "react";
-import LoginSample from "./Loginsample";
+import LoginSample from "./LoginSample";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
