357cc597f824307f24f05b292c5d6b2ff59a1c7c
Uzair Ejaz <uzairejaz987@gmail.com>
2025-10-27 22:35:04 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider , Navigate } from "react-router-dom";
 
 import App from "./App.jsx";
 import Login from "./Pages/Auth/Login";
@@ -8,7 +8,6 @@ import SignUp from "./Pages/Auth/SignUp";
 import DriverRegister from "./Pages/Auth/DriverRegister";
 import OperatorRegister from "./Pages/Auth/OperatorRegister";
 import roleRoutes from "./Routes/RoleRoutes";
-import Navigate from "react"
 import "./index.css";
 import "./aws-config"; // Import your Amplify config here
 
