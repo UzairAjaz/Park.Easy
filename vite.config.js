ee75430bbc0f36e3d71ccb0e8a0a53597c70ac7e
Uzair Ejaz <uzairejaz987@gmail.com>
2025-10-27 20:42:09 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/', 
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve('./src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
